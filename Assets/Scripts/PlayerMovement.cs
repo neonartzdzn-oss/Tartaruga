616e011191b6af23020bf355134ca917420a9ba1
neonartzdzn-oss <neonartzdzn@gmail.com>
2026-04-05 00:46:40 -0300

    atualização

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,45 +1,116 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class MovimentoJogador : MonoBehaviour
 {
-    public float speed = 5f;
 
-    private Rigidbody rb;
+    public Rigidbody rb;
+    public Transform cam;
+    public LayerMask ground;
 
-    private Vector3 movement;
+    public float speed, maxSpeed, drag;
+    public float rotationSpeed, jumpForce;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    bool left, forward, backward, right;
+    bool grounded, jump;
 
-    // Update is called once per frame
     void Update()
     {
-        Getinput(); 
+        HandleInput();
+        LimitVelocity();
+        CheckGrounded();
+
     }
 
-    private void Getinput()
+    void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
 
-        movement = new Vector3(horizontal, 0f, vertical);
+        HandleMovement();
+        HandleRotation();
+
     }
 
-    private void FixedUpdate()
+    void CheckGrounded()
     {
-        Move();
+        grounded = Physics.Raycast(transform.position + Vector3.up * .1f, Vector3.down, .2f, ground);
     }
 
-    private void Move()
+    void HandleRotation()
     {
-        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        if ((new Vector2(rb.velocity.x, rb.velocity.z)).magnitude > .1f)
+        {
+            Vector3 horizontalDir = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            Quaternion rotation = Quaternion.LookRotation(horizontalDir, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed);
+        }
     }
+
+    void HandleDrag()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z) / (1 + drag / 100) + new Vector3(0, rb.velocity.y, 0);
+    }
+
+    void LimitVelocity()
+    {
+        Vector3 horizontalVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        if (horizontalVelocity.magnitude > maxSpeed)
+        {
+            Vector3 limitedVelocity = horizontalVelocity.normalized * maxSpeed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+
+        }
+    }
+
+    void HandleMovement()
+    {
+        Quaternion dir = Quaternion.Euler(0f, cam.rotation.eulerAngles.y, 0f);
+
+        if (left)
+        {
+            rb.AddForce(dir * Vector3.left * speed);
+            left = false;
+        }
+
+        if (forward)
+        {
+            rb.AddForce(dir * Vector3.forward * speed);
+            forward = false;
+        }
+        if (backward)
+        {
+            rb.AddForce(dir * Vector3.back * speed);
+            backward = false;
+        }
+        if (right)
+        {
+            rb.AddForce(dir * Vector3.right * speed);
+            right = false;
+        }
+        if (jump && grounded)
+        {
+            transform.position += Vector3.up * .1f;
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.y);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jump = false;
+        }
+
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKey(KeyCode.A))
+            left = true;
+        if (Input.GetKey(KeyCode.W))
+            forward = true;
+        if (Input.GetKey(KeyCode.S))
+            backward = true;
+        if (Input.GetKey(KeyCode.D))
+            right = true;
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+            jump = true;
+    }
+    // Update is called once per frame
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -48,6 +119,4 @@ public class PlayerMovement : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-
 }
